1b04c8b6aef801d7a6bee3dbd82e7edf5819d118
deaddog <mikkelslsl@gmail.com>
2015-11-30 08:32:07 +0100

    Applied the Connector structure.

--- AttackTree/GraphBuilder.cs
+++ AttackTree/GraphBuilder.cs
@@ -21,17 +21,30 @@ namespace AttackTree
         {
             Vector2D pagesize;
             var nodes = Node.GetNodes(dot, out pagesize);
+            var links = Link.GetLinks(dot, nodes);
 
             PDFDocument doc = new PDFDocument();
             var page = doc.Pages.Add(new Page(pagesize));
 
-            for (int i = 0; i < nodes.Length; i++)
-                page.Objects.AddRange(HandleNode(nodes[i]));
-
-            var links = Link.GetLinks(dot, nodes);
+            var space = new Vector1D(0.15, UnitsOfMeasure.Centimeters);
 
             for (int i = 0; i < links.Length; i++)
-                page.Objects.AddRange(Handlelink(links[i]));
+            {
+                var link = links[i];
+
+                var allTo = links.Where(x => x.To == link.To).ToList();
+                var allFrom = links.Where(x => x.From == link.From).ToList();
+
+                var p1 = new Vector2D(link.From.Center.X, link.From.GateRect.Bottom);
+                var p3 = new Vector2D(link.To.Center.X, link.To.BoxRect.Top);
+
+                p1.X += allFrom.IndexOf(link) * space - ((allFrom.Count - 1) * space) / 2;
+
+                page.Objects.Add(new Connector(p1, p3 - p1, link.From.Center.X));
+            }
+
+            for (int i = 0; i < nodes.Length; i++)
+                page.Objects.AddRange(HandleNode(nodes[i]));
 
             doc.Create(outputpath, open);
         }
@@ -45,41 +58,12 @@ namespace AttackTree
             {
                 case Node.NodeTypes.AND:
                     yield return new AndGate(node.GateSize.Y) { Offset = node.GateOffset };
-
-                    yield return new Line(new Vector2D(Vector1D.Zero, node.GateRect.Top - node.BoxRect.Bottom))
-                    { Offset = new Vector2D(node.Center.X, node.BoxRect.Bottom) };
                     break;
 
                 case Node.NodeTypes.OR:
                     yield return new OrGate(node.GateSize.Y) { Offset = node.GateOffset };
-
-                    yield return new Line(new Vector2D(Vector1D.Zero, node.GateRect.Top - node.BoxRect.Bottom))
-                    { Offset = new Vector2D(node.Center.X, node.BoxRect.Bottom) };
-                    break;
-                default:
-                    //yield return new Box(node.Size) { Offset = node.Center - node.Size / 2, FillColor = node.NodeType == Node.NodeTypes.OR ? Color.OrangeRed : Color.GreenYellow };
                     break;
             }
-
-            //yield return new Box(node.Size) { Offset = node.Center - node.Size / 2, FillColor = node.NodeType == Node.NodeTypes.OR ? Color.OrangeRed : Color.GreenYellow };
-        }
-        static IEnumerable<PDFObject> Handlelink(Link link)
-        {
-            var p1 = new Vector2D(link.From.Center.X, link.From.GateRect.Bottom);
-            var p3 = new Vector2D(link.To.Center.X, link.To.BoxRect.Top);
-
-            var size = p3 - p1;
-            var p2 = p3 - new Vector2D(Vector1D.Zero, size.Y / 2);
-
-            if (link.From.NodeType == Node.NodeTypes.Unknown)
-            {
-                var p1_ = new Vector2D(link.From.Center.X, link.From.BoxRect.Bottom);
-                yield return new Line(new Vector2D(Vector1D.Zero, (p2 - p1_).Y)) { Offset = p1_ };
-            }
-            else
-                yield return new Line(new Vector2D(Vector1D.Zero, size.Y / 2)) { Offset = p1 };
-            yield return new Line(new Vector2D(-size.X, Vector1D.Zero)) { Offset = p2 };
-            yield return new Line(new Vector2D(Vector1D.Zero, size.Y / 2)) { Offset = p2 };
         }
     }
 }
